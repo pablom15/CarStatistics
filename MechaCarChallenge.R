858dbbda7e6db28a4377ca14c407b9c4a9b44470
pablom15 <pablo.maldo15@gmail.com>
2020-09-13 10:35:52 -0500

    Challenge files

---
+++ MechaCarChallenge.R
@@ -0,0 +1,42 @@
+# Import mecha car data
+Mecha_Car <- read.csv('MechaCar_mpg.csv',stringsAsFactors = F) #read in dataset
+head(Mecha_Car)
+
+# Scatter plot of Vehicle Length vs MPG to get an idea of the data
+plt <- ggplot(Mecha_Car,aes(x=vehicle.length,y=mpg))
+plt + geom_point()
+
+# Generate multiple linear regression model
+lm(mpg ~ vehicle.length + vehicle.weight + spoiler.angle + ground.clearance,data=Mecha_Car)
+
+# Generate summary statistics on multiple linear regression model
+summary(lm(mpg ~ vehicle.length + vehicle.weight + spoiler.angle + ground.clearance,data=Mecha_Car))
+
+# Suspension Coil Summary
+Coil <- read.csv('Suspension_Coil.csv',stringsAsFactors = F) #read in dataset
+head(Coil)
+
+# Summary Stats for coil pressure data
+summary(Coil$PSI)
+
+# Creates a summary table and view the table
+Coil_summary <- Coil %>% summarize(Min=min(PSI),
+                                   Max=max(PSI),
+                                   Mean=mean(PSI),
+                                   Median=median(PSI),
+                                   Variance=var(PSI),
+                                   StdDev=sd(PSI),
+                                   n=n())
+View(Coil_summary)
+
+ggplot(Coil,aes(x=PSI)) + geom_density() #visualize distribution using density plot
+
+shapiro.test(Coil$PSI)
+
+coilSample <- Coil %>% sample_n(100)
+mean(coilSample$PSI)
+
+t.test(coilSample$PSI, mu=1500, alternative = 'two.sided')
+
+
+
